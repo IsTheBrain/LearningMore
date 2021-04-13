7ac2a2559e6d049f15c464d097ce71d0adf0ab47
Brini, Michele <michele.brini@juliusbaer.com>
2021-04-13 15:40:15 +0200

    few u commit twice

--- appFS.js
+++ appFS.js
@@ -5,4 +5,36 @@ fs.writeFile('example.txt', "this is example", (err) => {
         console.log(err);
     else
         console.log('successfully created');
+    fs.readFile('example.txt', 'utf8', (err, file) => {
+        if (err) {
+            console.log('errors occurred');
+        } else {
+            console.log(file);
+        }
+    }
+    );
+});
+
+fs.appendFile('newExample.txt', 'Data new', (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('did it');
+    }
+});
+
+//fs.rename('example.txt', 'newExample.txt', (err) => {
+//    if (err) {
+//        console.log(err);
+//    } else {
+//        console.log('rename with no error. good');
+//    }
+//});
+
+fs.unlink('newExample.txt', (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('deleted correctly');
+    }
 });
